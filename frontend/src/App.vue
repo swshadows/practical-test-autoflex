28db0be6d8c85163c03c9239fe14ebd6e1e0a468
Joe <joelson.jpl@gmail.com>
2026-02-04 19:45:38 -0400

    feat: initializing frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const today = new Date().getFullYear()
+</script>
+
+<template>
+  <div class="h-dvh grid grid-rows-[5%_1fr_5%]">
+    <header class="px-2 py-1 bg-black flex justify-between items-center">
+      <h2 class="text-white font-bold">Teste Técnico - Autoflex</h2>
+      <div class="flex gap-1">
+        <RouterLink to="/products">
+          <button
+            class="py-1 px-2 rounded bg-white hover:bg-neutral-500 cursor-pointer hover:text-white transition flex justify-between items-center gap-1"
+          >
+            <i class="nf nf-fa-box"></i>
+            <span>Produtos</span>
+          </button>
+        </RouterLink>
+        <RouterLink to="/materials">
+          <button
+            class="py-1 px-2 rounded bg-white hover:bg-neutral-500 cursor-pointer hover:text-white transition flex justify-between items-center gap-1"
+          >
+            <i class="nf nf-md-dots_triangle"></i>
+            <span>Materiais</span>
+          </button>
+        </RouterLink>
+      </div>
+    </header>
+    <RouterView></RouterView>
+    <footer class="px-2 py-1 bg-black flex justify-between items-center text-white">
+      <span
+        >Criado por
+        <a
+          class="text-blue-400 font-bold hover:underline"
+          href="https://github.com/swshadows/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >@swshadows</a
+        ></span
+      >
+      <span>{{ today }}</span>
+    </footer>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-full h-full flex flex-col align-center justify-center text-center">
+    <h2 class="text-4xl font-bold text-neutral-400">Bem-vindo ao app do teste técnico</h2>
+    <span class="text-lg italic">Use os botões no topo direito pra acessar as telas</span>
+  </div>
+</template>
